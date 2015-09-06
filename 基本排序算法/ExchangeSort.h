c78830822cd6cfb7752fb666122a86399bbb6f87
wdlin <460799951@qq.com>
2015-09-06 22:38:45 +0800

    repair bubble sort

--- 基本排序算法/ExchangeSort.h
+++ 基本排序算法/ExchangeSort.h
@@ -9,7 +9,7 @@
 1.比较相邻的元素。如果第一个比第二个大，就交换他们两个。
 2.对每一对相邻元素作同样的工作，从开始第一对到结尾的最后一对。在这一点，最后的元素应该会是最大的数。
 3.针对所有的元素重复以上的步骤，除了最后一个。
-4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要比较。
+4.持续每次对越来越少的元素重复上面的步骤，直到没有任何一对数字需要交换。
 
 */
 
@@ -18,6 +18,7 @@ void bubbleSort(T array[],int size)
 {
 	for (int i = 0; i < size - 1; i++)
 	{
+		bool flag = true;
 		for (int j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j]>array[j + 1])
@@ -25,8 +26,11 @@ void bubbleSort(T array[],int size)
 				T temp = array[j + 1];
 				array[j + 1] = array[j];
 				array[j] = temp;
+				flag = false;
 			}
 		}
+		if (flag)//没有交换操作，表示是有序的，结束排序
+			break;
 	}
 }
 
