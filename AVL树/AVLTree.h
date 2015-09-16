a6ed1de52bfcb42b1d527eb98f4dc0d71d4dd0a9
wdlin <460799951@qq.com>
2015-09-16 10:54:13 +0800

    red black tree

--- AVL树/AVLTree.h
+++ AVL树/AVLTree.h
@@ -7,7 +7,7 @@
 
 /*
 AVL树 最简单的平衡搜索二叉树
-参考数据结构与算法分析（C++描述） P014 - P107
+参考数据结构与算法分析（C++描述） P104 - P107
 */
 template<class Comparable>
 class AVLTree
@@ -17,6 +17,8 @@ public:
 	~AVLTree();
 	void print();
 	void insert(const Comparable& x);
+	bool remove(const Comparable& x);
+	bool contains(const Comparable& x);
 private:
 	typedef struct AVLNode//树节点类型
 	{
@@ -30,6 +32,10 @@ private:
 	AVLNode* root;//根节点
 	int height(AVLNode* node) const;//返回节点的高度
 	void insert(const Comparable& x, AVLNode*& root);//插入节点
+	AVLNode* remove(AVLNode* node , const Comparable& x);//移除节点，返回根
+	AVLNode* find(const Comparable& x);//查找节点，返回节点
+	AVLNode* findMin(AVLNode* node);//
+	AVLNode* findMax(AVLNode* node);//
 	void rotateWithLeftChild(AVLNode*& k2);//左左
 	void rotateWithRightChild(AVLNode*& k1);//右右
 	void doubleWithLeftChild(AVLNode*& k3);//左右
@@ -73,6 +79,12 @@ int AVLTree<Comparable>::height(AVLNode* node) const
 }
 
 template<class Comparable>
+bool AVLTree<Comparable>::contains(const Comparable& x)
+{
+	return (find(x) == nullptr) ? false : true;
+}
+
+template<class Comparable>
 void AVLTree<Comparable>::insert(const Comparable& x, AVLNode*& node)
 {
 	if (node == nullptr)
@@ -82,10 +94,10 @@ void AVLTree<Comparable>::insert(const Comparable& x, AVLNode*& node)
 		insert(x, node->left);
 		if (height(node->left) - height(node->right) == 2)
 		{
-			if (x < node->left->element)
+			if (x < node->left->element)//左左
 				rotateWithLeftChild(node);
 			else
-				doubleWithLeftChild(node);
+				doubleWithLeftChild(node);//左右
 		}
 	}
 	else if (x > node->element)//插入右子树
@@ -93,15 +105,119 @@ void AVLTree<Comparable>::insert(const Comparable& x, AVLNode*& node)
 		insert(x, node->right);
 		if (height(node->right) - height(node->left) == 2)
 		{
-			if (x > node->right->element)
+			if (x > node->right->element)//右右
 				rotateWithRightChild(node);
 			else
-				doubleWithRightChild(node);
+				doubleWithRightChild(node);//右左
 		}
 	}
 	else
 		;//重复，不操作
 	node->height = std::max(height(node->left),height(node->right)) + 1;
+}
+
+template<class Comparable>
+bool AVLTree<Comparable>::remove(const Comparable& x)
+{
+	if (find(x) == nullptr)
+		return false;
+	else
+		root = remove(root, x);
+	return true;
+}
+
+/*
+删除节点的实际函数，输入树的根和要删除的值。返回删除后的树根。
+由删除接口函数调用，默认是要删除的值一定在树中。
+*/
+template<class Comparable>
+typename AVLTree<Comparable>::AVLNode* AVLTree<Comparable>::remove(AVLNode* node, const Comparable& x)
+{
+	//要删除的节点就是当前的根
+	if (x == node->element)
+	{	//左右子树非空
+		if (node->left != nullptr && node->right != nullptr)
+		{	//如果左子树大，删除左子树中元素值最大的那个节点，同时将其值赋值给根节点 
+			if (height(node->left) > height(node->right))
+			{
+				node->element = findMax(node->left)->element;
+				node->left = remove(node->left, node->element);
+			}//如果右子树大，删除右子树中元素值最小的那个节点，同时将其值赋值给根节点
+			else
+			{
+				node->element = findMin(node->right)->element;
+				node->right = remove(node->right, node->element);
+			}
+		}
+		else//至少有一个孩子的空的，用空的孩子的另一个兄弟替换（可能也是空的）
+		{
+			AVLNode* oldNode = node;
+			node = (node->left != nullptr) ? node->left : node->right;
+			delete oldNode;
+		}
+		if(node!=nullptr)//更新高度，node可能是空的
+			node->height = max(height(node->left), height(node->right)) + 1;
+	}
+	else if (x < node->element)//删除节点在左子树
+	{	//在左子树中进行递归删除
+		node->left = remove(node->left, x);
+		if (height(node->right) - height(node->left)>1)//判断是否仍然平衡条件
+		{
+			if (height(node->right->left) > height(node->right->right))//右左
+				doubleWithRightChild(node);
+			else
+				rotateWithRightChild(node);//右右
+		}
+		node->height = max(height(node->left), height(node->right)) + 1;//更新高度
+	}
+	else//删除节点在右子树
+	{//在右子树中进行递归删除
+		node->right = remove(node->right, x);
+		if (height(node->left) - height(node->right)>1)//判断是否仍然平衡条件
+		{
+			if (height(node->left->right) > height(node->left->left))//左右
+				doubleWithLeftChild(node);
+			else
+				rotateWithLeftChild(node);//右右
+		}
+		node->height = max(height(node->left), height(node->right)) + 1;
+	}
+	return node;
+}
+
+template<class Comparable>
+typename AVLTree<Comparable>::AVLNode* AVLTree<Comparable>::findMin(AVLNode* node)
+{
+	if (node != nullptr)
+	while (node->left != nullptr)
+		node = node->left;
+	return node;
+}
+
+template<class Comparable>
+typename AVLTree<Comparable>::AVLNode* AVLTree<Comparable>::findMax(AVLNode* node)
+{
+	if (node!=nullptr)
+	while (node->right != nullptr)
+		node = node->right;
+	return node;
+}
+template<class Comparable>
+typename AVLTree<Comparable>::AVLNode* AVLTree<Comparable>::find(const Comparable& x)
+{
+	if (root == nullptr)
+		return nullptr;
+	AVLNode* temp = root;
+	while (temp != nullptr)
+	{
+		if (x == temp->element)
+			return temp;
+		if (x < temp->element)
+			temp = temp->left;
+		else
+			temp = temp->right;
+	}
+	return nullptr;
 }
 
 template<class Comparable>
@@ -179,7 +295,7 @@ void AVLTree<Comparable>::print()
 		}
 		else
 		{
-			cout << p->element<<",";
+			cout << p->element<<"("<<p->height<<")"<<",";
 			if (addCount + 2 <= maxnum)
 			{
 				queue.push(p->left);
--- AVL树/Main.cpp
+++ AVL树/Main.cpp
@@ -1,25 +1,31 @@
 #include <iostream>
+#include <ctime>
 #include "AVLTree.h"
 using namespace std;
 
 int main()
 {
+	srand(time(NULL));
 	AVLTree<int> tree;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 25; i++)
 	{
-		tree.insert(rand() % 100);
+		tree.insert(rand() % 10);
 	}
-	
 	tree.print();
+	for (int i = 0; i < 15; i++)
+	{
+		tree.remove(rand() % 10);
+	}
+
 	system("pause");
 	return 0;
 }
 
 /*
 Tree:
-41,
-24,67,
-0,34,62,69,
-NL,NL,NL,NL,58,64,NL,78,
+4(3),
+2(1),8(2),
+1(0),3(0),6(1),9(0),
+NL,NL,NL,NL,5(0),7(0),NL,NL,
 请按任意键继续. . .
 */
